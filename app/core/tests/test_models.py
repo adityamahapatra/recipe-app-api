8e0231740807932be87f02ab3a09cb59060a8abd
adityamahapatra <adi.mahapatra@gmail.com>
2021-01-06 05:29:33 +0530

    Add a test to check new user creation with an email.

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """Test if creating a new user with an email is successful."""
+        email = "test.user@gmail.com"
+        password = "testpass123"
+        user = get_user_model().objects.create_user(
+            email=email, password=password
+        )
+
+        self.assertEquals(user.email, email)
+        self.assertTrue(user.check_password(password))
