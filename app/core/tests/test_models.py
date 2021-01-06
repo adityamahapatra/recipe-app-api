94aad2fbe010c5b35e4d0b527fdb246fcd6e44aa
adityamahapatra <adi.mahapatra@gmail.com>
2021-01-06 08:12:18 +0530

    Add type annotations.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -4,7 +4,7 @@ from django.contrib.auth import get_user_model
 
 class ModelTests(TestCase):
 
-    def test_create_user_with_email_successful(self):
+    def test_create_user_with_email_successful(self) -> None:
         """Test if creating a new user with an email is successful."""
         email = "test.user@gmail.com"
         password = "testpass123"
@@ -15,7 +15,7 @@ class ModelTests(TestCase):
         self.assertEquals(user.email, email)
         self.assertTrue(user.check_password(password))
 
-    def test_new_user_email_normalized(self):
+    def test_new_user_email_normalized(self) -> None:
         """Test if the email for a new user is normalized."""
         email = "test.user@GMAIL.COM"
         user = get_user_model().objects.create_user(
@@ -24,14 +24,14 @@ class ModelTests(TestCase):
 
         self.assertEquals(user.email, email.lower())
 
-    def test_new_user_email_validity(self):
+    def test_new_user_email_validity(self) -> None:
         """Test if creating a new user without an email raises an error."""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(
                 email=None, password="testpass123"
             )
 
-    def test_create_new_super_user(self):
+    def test_create_new_super_user(self) -> None:
         """Test creating a new super user."""
         email = "test.super.user@gmail.com"
         password = "testpass123"
